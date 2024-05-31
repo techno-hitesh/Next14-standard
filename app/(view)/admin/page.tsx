bee71ecd26b6e9366fd23f4ca56d8691e4fe18e5
jaspreet kaur <jaspreet.kaur@technocratshorizons.com>
2024-05-31 18:18:11 +0530

    uncomment chart code

--- app/(view)/admin/page.tsx
+++ app/(view)/admin/page.tsx
@@ -1,9 +1,9 @@
 "use client"
 import React, { useEffect } from 'react';
-// import Radialchart from './components/radialchart';
-// import Piechart from './components/piechart';
+import Radialchart from './components/radialchart';
+import Piechart from './components/piechart';
+import Barchart from './components/barchart';
 import Transcation from './components/transcation';
-// import Barchart from './components/barchart';
 
 const AdminPage = () => {
 
@@ -11,9 +11,9 @@ const AdminPage = () => {
     <>
       <div className='flex flex-col '>
         <div className='grid gap-3 grid-cols-3'>
-          {/* <Barchart />
+          <Barchart />
           <Piechart />
-          <Radialchart /> */}
+          <Radialchart />
         </div>
         <div className='flex flex-col border border-gray-200 rounded-md shadow-md shadow-gray-200 p-4 mt-5'>
           <Transcation />
